574dee1f40223502bf2188e0f52d871cd88db641
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-04-24 02:46:31 -0400

    prompt

--- llm.py
+++ llm.py
@@ -2,6 +2,10 @@ import uuid
 from pydantic import Field, BaseModel
 
 
+class Prompt(BaseModel):
+    prompt: str
+
+
 class SummaryQuery(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
     post_id: str
