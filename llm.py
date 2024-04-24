252ca807f0257cca573a63c77348067895bcffab
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-04-24 17:17:17 -0400

    llm

--- llm.py
+++ llm.py
@@ -6,25 +6,18 @@ class Prompt(BaseModel):
     prompt: str
 
 
-class SummaryQuery(BaseModel):
+class PostQuery(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
     post_id: str
     text: str
 
 
-class Summary(BaseModel):
+class PostCompletion(BaseModel):
+    title: str = Field(description="title of the news event")
+    summary: str = Field(description="summary of the news event")
+
+
+class PostAnalysis(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
     post_id: str
-    summary: str
-
-
-class TitleQuery(BaseModel):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    post_id: str
-    text: str
-
-
-class Title(BaseModel):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    post_id: str
-    title: str
+    completion: PostCompletion
