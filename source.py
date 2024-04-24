ff746ec764a58d20997f542b430dc7cb1b904067
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-04-24 04:01:54 -0400

    source

--- source.py
+++ source.py
@@ -4,11 +4,11 @@ from pydantic import BaseModel, Field
 
 class Source(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    title: str
-    link: str
-    media: str
-    author: str
-    date: str
+    title: str = "Unknown title"
+    link: str = "Unknown link"
+    media: str = "Unknown media"
+    author: str = "Unknown author"
+    date: str = "Unknown date"
 
     class Config:
         populate_by_name = True
