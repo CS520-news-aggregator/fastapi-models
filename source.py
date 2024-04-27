0893a50d8a60f981d926477379b5991a8453ea8e
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-04-27 04:13:20 -0400

    changed id

--- source.py
+++ source.py
@@ -3,12 +3,12 @@ from pydantic import BaseModel, Field
 
 
 class Source(BaseModel):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    title: str = "Unknown title"
-    link: str = "Unknown link"
-    media: str = "Unknown media"
-    author: str = "Unknown author"
-    date: str = "Unknown date"
+    id: str = Field(lambda: str(uuid.uuid4()), alias="_id")
+    title: str = ""
+    link: str = ""
+    media: str = ""
+    author: str = ""
+    date: str = ""
 
     class Config:
         populate_by_name = True
