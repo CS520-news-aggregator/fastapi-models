e3bc23a3dfe78a93545519715f3ceab100a33b44
Ji Cao <caoji2002@hotmail.com>
2024-05-08 22:39:29 -0400

    More complex spacy

--- utils/constants.py
+++ utils/constants.py
@@ -5,6 +5,7 @@ OLLAMA_HOST = os.getenv("OLLAMA_HOST", "localhost") + ":11434"
 SCRAPER_HOST = os.getenv("SCRAPER_HOST", "localhost") + ":8050"
 RECOMMENDER_HOST = os.getenv("RECOMMENDER_HOST", "localhost") + ":8030"
 LLM_HOST = os.getenv("LLM_HOST", "localhost") + ":8040"
+LAPLACE_CONSTANT = 0.1
 
 LIST_TOPICS = [
     "World News",
