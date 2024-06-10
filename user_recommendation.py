9074728acdb40c319d76d1232aba705f063e7240
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-06-10 11:04:13 +0530

    added user recom

--- user_recommendation.py
+++ user_recommendation.py
@@ -9,5 +9,6 @@ class UserRecommendationQuery(BaseModel):
 
 class UserRecommendation(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), alias="_id")
+    user_id: str
     post_scores: List[str, float]
     date: str
